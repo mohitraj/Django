dd38a920356d4ce2b1ca24f058b01315a3b7de95
Mohit raj <mohitraj.cs@gmail.com>
2020-12-09 23:05:26 +0530

    Last day of Django

--- myproject_att/settings.py
+++ myproject_att/settings.py
@@ -156,5 +156,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'greatextreme@gmail.com'
-EMAIL_HOST_PASSWORD= 'bqianodvvdxmurxb'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD= ''
